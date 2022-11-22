328b9f12c6ea70c445b97acbf79065ecd4810e69
Lenard Frommelt <lenny09918050@gmail.com>
2022-11-22 11:40:00 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -3,8 +3,8 @@ module.exports = {
         return {
             "cached-persistentstate": "^0.0.1",
             "key-block": "^0.0.1",
-            "memory-decay": "^0.0.2",
-            "secret-manager-crypto-utils": "^0.1.1",
+            "memory-decay": "^0.0.3",
+            "secret-manager-crypto-utils": "^0.2.0",
             "thingy-schema-validate": "^0.0.1",
     	    "thingy-sci-ws-base":"^0.0.1"
         }
